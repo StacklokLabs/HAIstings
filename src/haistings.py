0024a35d2520d2ace4dbca18ed76250a28b5c76f
Juan Antonio Osorio <ozz@stacklok.com>
2025-02-14 12:42:19 +0200

    Add k8s report
    Signed-off-by: Juan Antonio Osorio <ozz@stacklok.com>

--- src/haistings.py
+++ src/haistings.py
@@ -5,62 +5,56 @@ if not os.environ.get("USER_AGENT"):
     os.environ["USER_AGENT"] = "HAIstings/0.0.1"
 
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains.llm import LLMChain
 from langchain.chat_models import init_chat_model
-from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import TextLoader, Load
 from langchain_core.prompts import ChatPromptTemplate
 
-loader = TextLoader("./report.txt")
-docs = loader.load()
+from haistings import k8sreport
 
-llm = init_chat_model("this-makes-no-difference-to-codegate",
-                      model_provider="openai",
-                      base_url="http://127.0.0.1:8989/v1/mux")
+if __name__ == "__main__":
+    llm = init_chat_model("this-makes-no-difference-to-codegate",
+                        model_provider="openai",
+                        # CodeGate Muxing API URL
+                        base_url="http://127.0.0.1:8989/v1/mux")
 
-assistant_text = """"You are a Software Security assistant. Your goal is to
-help infrastructure engineerings to secure their deployments. You are
-tasked with prioritizing what software to update first. You have a list of
-container image references with their list of vulnerabilities. You also have a list of known
-vulnerabilities and their severity. Your goal is to write a concise summary
-that's actionable and informative.
+    assistant_text = """"You are a Software Security assistant. Your goal is to
+    help infrastructure engineerings to secure their deployments. You are
+    tasked with prioritizing what software to update first. You have a list of
+    container image references with their list of vulnerabilities. You also have a list of known
+    vulnerabilities and their severity. Your goal is to write a concise summary
+    that's actionable and informative.
 
-Start the report with a Hasting's sounding introduction. Then, provide a
-summary of the vulnerabilities in the container images.
+    Start the report with a Hasting's sounding introduction. Then, provide a
+    summary of the vulnerabilities in the container images.
 
-ONLY provide the information that is relevant to the task at hand. Do not
-provide extraneous information. Your summary should be clear, concise, and
-easy to understand. Make sure to prioritize the software components based on
-the severity of the vulnerabilities, the impact on the infrastructure, and
-the reachability of the vulnerability.
+    ONLY provide the information that is relevant to the task at hand. Do not
+    provide extraneous information. Your summary should be clear, concise, and
+    easy to understand. Make sure to prioritize the software components based on
+    the severity of the vulnerabilities, the impact on the infrastructure, and
+    the reachability of the vulnerability.
 
-Note that your chatacter is based on a fictional persona that resembles Arthur
-Hastings, a character from the Agatha Christie's Poirot series. You are
-intelligent, meticulous, and have a keen eye for detail. You are also
-methodical and systematic in your approach. You are not afraid to ask
-questions and seek clarification when needed. You are also a good listener
-and have a knack for understanding complex technical concepts.
+    Note that your chatacter is based on a fictional persona that resembles Arthur
+    Hastings, a character from the Agatha Christie's Poirot series. You are
+    intelligent, meticulous, and have a keen eye for detail. You are also
+    methodical and systematic in your approach. You are not afraid to ask
+    questions and seek clarification when needed. You are also a good listener
+    and have a knack for understanding complex technical concepts.
 
-End the report with a closing statement that also sounds like something
-Hastings would say.
-"""
+    End the report with a closing statement that also sounds like something
+    Hastings would say.
+    """
 
-# Define prompt
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", assistant_text),
-        ("user", "What container images should be updated first? Do the priorization based on the following context:\n\n{context}"),
-    ],
-)
+    # Define prompt
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", assistant_text),
+            ("user", "What container images should be updated first? Do the priorization based on the following context:\n\n{context}"),
+        ],
+    )
 
-# Define the chain
-chain = create_stuff_documents_chain(llm, prompt)
+    report = k8sreport.buildVulnerabilityReport(5)
 
-# Run the chain
-# Note doing a single call as this does not work.
-# result = chain.invoke({"context": docs})
-# print(result)
-
-for tok in chain.stream(
-    {"context": docs},
-):
-    print(tok, end="")
+    for tok in llm.stream(
+        prompt.invoke({"context": report}),
+    ):
+        print(tok, end="")
---
+++ src/k8sreport.py
@@ -0,0 +1,136 @@
+import json
+import sh
+
+from typing import List
+
+
+class VulnInfo:
+    def __init__(self, id: str, title: str, severity: str):
+        self.id = id
+        self.title = title
+        self.severity = severity
+    
+    def __str__(self):
+        return """
+  - ID: {}
+    Title: {}
+    Severity: {}""".format(self.id, self.title, self.severity)
+
+
+class ImageWithVulns:
+    def __init__(self, srv: str, img: str, digest: str, tag: str, criticalVulns: int, highVulns: int, namespace: str, vulns: List[VulnInfo] = []):
+        self.srv = srv
+        self.img = img
+        self.tag = tag
+        self.digest = digest
+        self.criticalVulns = criticalVulns
+        self.highVulns = highVulns
+        self.namespace = namespace
+        self.vulns = vulns
+
+    def __str__(self):
+        if self.tag is None:
+            return """image: {}/{}@{}"
+namespace: {}
+Critical vulns: {}
+High vulns: {}
+Vulnerabilities: {}
+""".format(self.srv, self.img, self.digest, self.namespace, self.criticalVulns, self.highVulns, "\n".join((str(v) for v in self.vulns)))
+        return """image: {}/{}:{}
+namespace: {}
+Critical vulns: {}
+High vulns: {}
+Vulnerabilities: {}
+""".format(self.srv, self.img, self.tag, self.namespace, self.criticalVulns, self.highVulns, "\n".join((str(v) for v in self.vulns)))
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        if not isinstance(other, ImageWithVulns):
+            return
+        return self.srv == other.srv and self.img == other.img and self.tag == other.tag and self.digest == other.digest
+
+    # uniqueness is based on image + tag + digest
+    def __hash__(self):
+        return hash((self.srv, self.img, self.tag, self.digest))
+
+    @classmethod
+    def hash(cls, srv: str, img: str, digest: str, tag: str):
+        return hash((srv, img, tag, digest))
+
+    # Sort by critical vulns, then by high vulns
+    # Critical vulns are 10 times more important than high vulns
+    def __lt__(self, other):
+        criticalImportance = 10
+        if self.criticalVulns * criticalImportance + self.highVulns < other.criticalVulns * criticalImportance + other.highVulns:
+            return True
+        return False
+
+
+def gatherVulns() -> tuple[set, int, int]:
+    vulnsjson = sh.kubectl("get", "vulns", "-A", "-o", "json")
+    vulns = json.loads(vulnsjson)
+
+    imgvulns = set()
+    totalCritical = 0
+    totalHigh = 0
+
+    for vuln in vulns["items"]:
+        if ImageWithVulns.hash(
+            vuln["report"]["registry"]["server"],
+            vuln["report"]["artifact"]["repository"],
+            vuln["report"]["artifact"]["digest"],
+            vuln["report"]["artifact"].get("tag")) in imgvulns:
+            continue
+        vulnList = getVulnList(vuln["report"]["vulnerabilities"])
+        img = ImageWithVulns(
+            vuln["report"]["registry"]["server"],
+            vuln["report"]["artifact"]["repository"],
+            vuln["report"]["artifact"]["digest"],
+            vuln["report"]["artifact"].get("tag"),
+            vuln["report"]["summary"]["criticalCount"],
+            vuln["report"]["summary"]["highCount"],
+            vuln["metadata"]["namespace"],
+            vulnList)
+        
+        totalCritical += img.criticalVulns
+        totalHigh += img.highVulns
+
+        imgvulns.add(img)
+
+    return imgvulns, totalCritical, totalHigh
+
+
+def getVulnList(vulns: list) -> List[VulnInfo]:
+    vulnList = []
+    for vuln in vulns:
+        vulnList.append(VulnInfo(vuln["vulnerabilityID"], vuln["title"], vuln["severity"]))
+    return vulnList
+
+
+def buildreport(imgvulns: set, top: int) -> str:
+    out = ""
+    out += "## Showing top {} images with most critical vulnerabilities".format(top)
+
+    if len(imgvulns) == 0:
+        out += "\nNo vulnerabilities found"
+        return out
+
+    if top > len(imgvulns):
+        top = len(imgvulns)
+
+    count = 0
+    for img in sorted(imgvulns, reverse=True):
+        out += img
+        if count == top:
+            break
+        count += 1
+
+    return out
+
+
+def buildVulnerabilityReport(top: int) -> str:
+    imgvulns, _, _ = gatherVulns()
+
+    return buildreport(imgvulns, top)
